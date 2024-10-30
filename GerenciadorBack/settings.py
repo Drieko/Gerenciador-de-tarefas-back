3f84b0ee7bf816c67f6ae3b907624234d12cbaae
Drieko <driekodev@gmail.com>
2024-10-30 19:54:28 -0300

    mucho texto

--- GerenciadorBack/settings.py
+++ GerenciadorBack/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +45,7 @@ INSTALLED_APPS = [
     'api',
 ]
 
-COS_ORIGIN_ALLOW = [
+CORS_ALLOWED_ORIGINS = [
     "https://teste-static-file.onrender.com"
 ]
 
@@ -81,11 +84,13 @@ WSGI_APPLICATION = 'GerenciadorBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DB_URL = config('DB_URL', default=os.environ.get('DB_URL',None))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DB_URL
+    )
+    
+    
 }
 
 
@@ -124,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
