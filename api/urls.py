44c9a28b6d2d0bc2fee62ab79980e3c0635fc9fa
Drieko <driekodev@gmail.com>
2024-10-30 21:12:43 -0300

    Arrumando as URLS, sim um nome decente after varios commits

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = {
+    path('api/', views.get_items, name='get_api'),
+    path('api/create/', views.create_item, name='create_api'),
+}
